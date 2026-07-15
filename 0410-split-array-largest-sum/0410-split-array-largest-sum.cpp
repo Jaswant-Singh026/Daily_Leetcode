f4892496931237efecb988e9215c22d2a414e583
jaswant singh <jaswantsin0526@gmail.com>
2026-07-16 00:03:26 +0530

    Time: 0 ms (100%), Space: 10.6 MB (10.31%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int canWesplit(vector<int>& nums, int maxSum, int maxSplit){
+    bool canWesplit(vector<int>& nums, int maxSum, int maxSplit){
         int split = 1;
         int curSum = 0;
         for(int i = 0; i < nums.size(); i++){
