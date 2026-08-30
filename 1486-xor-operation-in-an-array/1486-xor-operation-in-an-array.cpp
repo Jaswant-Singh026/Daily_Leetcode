48c9bbb1883575fbd8758ceff20900187b41fc18
jaswant singh <jaswantsin0526@gmail.com>
2026-08-30 10:49:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (78.99%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
+    int xorTil(int n){
+        if(n % 4 == 0) return n;
+        else if(n % 4 == 1) return 1;
+        else if(n % 4 == 2) return n+1;
+        else return 0;
+    }
     int xorOperation(int n, int start) {
-       int ans = 0;
-       for(int i = 0; i < n; i++){
-           ans ^= start + 2 * i;
-       } 
-       return ans;
+       int a = start >> 1;
+       int result = xorTil(a + n - 1) ^ xorTil(a - 1);
+       result = result << 1;
+
+       if((start & 1)&&(n & 1)){
+        result ^= 1;
+       }
+       return result;
     }
 };
