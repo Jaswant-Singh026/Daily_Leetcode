13c611c11bc8112a5cd5833affee92b50cb81196
jaswant singh <jaswantsin0526@gmail.com>
2026-09-16 00:52:56 +0530

    Time: 50 ms (91.27%), Space: 193.5 MB (54.87%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> NSE(vector<int>& arr, int n) {
+    vector<int> ans(n);
+    stack<int> st;
+
+        for(int i = n-1; i >= 0; i--) {
+
+            while(!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = st.empty() ? n : st.top();
+
+            st.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> PSE(vector<int>& arr, int n) {
+    vector<int> ans(n);
+    stack<int> st;
+
+        for(int i = 0; i < n; i++) {
+
+            while(!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+
+            ans[i] = st.empty() ? -1 : st.top();
+
+            st.push(i);
+        }
+        return ans;
+    }
+
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> nse = NSE(arr, n);
+        vector<int> pse = PSE(arr, n);
+
+        long long total = 0;
+        int mod = 1e9 + 7;
+
+        for(int i = 0; i < n; i++){
+            int left = i - pse[i];
+            int right = nse[i] - i;
+
+            total = (total + (right*left*1LL*arr[i])%mod)%mod;
+        }
+        return total;
+    }
+};
